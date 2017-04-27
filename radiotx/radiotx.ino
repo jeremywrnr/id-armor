47131fc52818215748f527378f3a5ad69e385ce8
Jeremy Warner <jeremywrnr@gmail.com>
2017-04-27 14:17:21 -0700

    radio tx code

--- radiotx/radiotx.ino
+++ radiotx/radiotx.ino
@@ -5,7 +5,7 @@
 // reliability, so you should only use RH_RF69  if you do not need the higher
 // level messaging abilities.
 // It is designed to work with the other example rf69_server.
-// Demonstrates the use of AES encryption, setting the frequency and modem 
+// Demonstrates the use of AES encryption, setting the frequency and modem
 // configuration
 
 #include <SPI.h>
@@ -18,58 +18,14 @@
 
 // Where to send packets to!
 #define DEST_ADDRESS   1
+
 // change addresses for each client board, any number :)
 #define MY_ADDRESS     2
 
-
-#if defined (__AVR_ATmega32U4__) // Feather 32u4 w/Radio
-  #define RFM69_CS      8
-  #define RFM69_INT     7
-  #define RFM69_RST     4
-  #define LED           13
-#endif
-
-#if defined(ARDUINO_SAMD_FEATHER_M0) // Feather M0 w/Radio
-  #define RFM69_CS      8
-  #define RFM69_INT     3
-  #define RFM69_RST     4
-  #define LED           13
-#endif
-
-#if defined (__AVR_ATmega328P__)  // Feather 328P w/wing
-  #define RFM69_INT     3  // 
-  #define RFM69_CS      4  //
-  #define RFM69_RST     2  // "A"
-  #define LED           13
-#endif
-
-#if defined(ESP8266)    // ESP8266 feather w/wing
-  #define RFM69_CS      2    // "E"
-  #define RFM69_IRQ     15   // "B"
-  #define RFM69_RST     16   // "D"
-  #define LED           0
-#endif
-
-#if defined(ESP32)    // ESP32 feather w/wing
-  #define RFM69_RST     13   // same as LED
-  #define RFM69_CS      33   // "B"
-  #define RFM69_INT     27   // "A"
-  #define LED           13
-#endif
-
-/* Teensy 3.x w/wing
-#define RFM69_RST     9   // "A"
 #define RFM69_CS      10   // "B"
-#define RFM69_IRQ     4    // "C"
-#define RFM69_IRQN    digitalPinToInterrupt(RFM69_IRQ )
-*/
- 
-/* WICED Feather w/wing 
-#define RFM69_RST     PA4     // "A"
-#define RFM69_CS      PB4     // "B"
-#define RFM69_IRQ     PA15    // "C"
-#define RFM69_IRQN    RFM69_IRQ
-*/
+#define RFM69_RST     11   // "A"
+#define RFM69_INT     6    // "D"
+#define LED           13
 
 // Singleton instance of the radio driver
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
@@ -80,12 +36,12 @@ RHReliableDatagram rf69_manager(rf69, MY_ADDRESS);
 
 int16_t packetnum = 0;  // packet counter, we increment per xmission
 
-void setup() 
+void setup()
 {
   Serial.begin(115200);
-  //while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
+  while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
 
-  pinMode(LED, OUTPUT);     
+  pinMode(LED, OUTPUT);
   pinMode(RFM69_RST, OUTPUT);
   digitalWrite(RFM69_RST, LOW);
 
@@ -97,7 +53,7 @@ void setup()
   delay(10);
   digitalWrite(RFM69_RST, LOW);
   delay(10);
-  
+
   if (!rf69_manager.init()) {
     Serial.println("RFM69 radio init failed");
     while (1);
@@ -115,9 +71,10 @@ void setup()
 
   // The encryption key has to be the same as the one in the server
   uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08
+                  };
   rf69.setEncryptionKey(key);
-  
+
   pinMode(LED, OUTPUT);
 
   Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
@@ -132,22 +89,22 @@ void loop() {
   delay(1000);  // Wait 1 second between transmits, could also 'sleep' here!
 
   char radiopacket[20] = "Hello World #";
-  itoa(packetnum++, radiopacket+13, 10);
+  itoa(packetnum++, radiopacket + 13, 10);
   Serial.print("Sending "); Serial.println(radiopacket);
-  
+
   // Send a message to the DESTINATION!
   if (rf69_manager.sendtoWait((uint8_t *)radiopacket, strlen(radiopacket), DEST_ADDRESS)) {
     // Now wait for a reply from the server
     uint8_t len = sizeof(buf);
-    uint8_t from;   
+    uint8_t from;
     if (rf69_manager.recvfromAckTimeout(buf, &len, 2000, &from)) {
       buf[len] = 0; // zero out remaining string
-      
+
       Serial.print("Got reply from #"); Serial.print(from);
       Serial.print(" [RSSI :");
       Serial.print(rf69.lastRssi());
       Serial.print("] : ");
-      Serial.println((char*)buf);     
+      Serial.println((char*)buf);
       Blink(LED, 40, 3); //blink LED 3 times, 40ms between blinks
     } else {
       Serial.println("No reply, is anyone listening?");
@@ -158,10 +115,10 @@ void loop() {
 }
 
 void Blink(byte PIN, byte DELAY_MS, byte loops) {
-  for (byte i=0; i<loops; i++)  {
-    digitalWrite(PIN,HIGH);
+  for (byte i = 0; i < loops; i++)  {
+    digitalWrite(PIN, HIGH);
     delay(DELAY_MS);
-    digitalWrite(PIN,LOW);
+    digitalWrite(PIN, LOW);
     delay(DELAY_MS);
   }
 }
